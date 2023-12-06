4d74ced1ec388ef31af7de29168d227aa23f8f8e
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-12-06 11:51:38 +0900

    Feat: 모든 국내 은행 추가

--- src/main/java/com/koreanguide/koreanguidebackend/domain/credit/data/enums/AccountProvider.java
+++ src/main/java/com/koreanguide/koreanguidebackend/domain/credit/data/enums/AccountProvider.java
@@ -1,7 +1,54 @@
 package com.koreanguide.koreanguidebackend.domain.credit.data.enums;
 
 public enum AccountProvider {
+//    경남은행
+    KYONGNAMBANK,
+//    광주은행
+    GWANGJUBANK,
+//    단위농협(지역축농협)
+    LOCALNONGHYEOP,
+//    부산은행
+    BUSANBANK,
+//    새마을금고
+    SAEMAUL,
+//    산림조합
+    SANLIM,
+//    신한은행
     SHINHAN,
+//    신협
+    SHINHYEOP,
+//    씨티은행
+    CITI,
+//    우리은행
+    WOORI,
+//    우체국예금보험
+    POST,
+//    저축은행중앙회
+    SAVINGBANK,
+//    전북은행
+    JEONBUKBANK,
+//    제주은행
+    JEJUBANK,
+//    카카오뱅크
+    KAKAOBANK,
+//    토스뱅크
+    TOSSBANK,
+//    하나은행
+    HANA,
+//    홍콩상하이은행
+    HSBC,
+//    IBK기업은행
+    IBK,
+//    KB국민은행
+    KOOKMIN,
+//    DGB대구은행
+    DAEGUBANK,
+//    KDB산업은행
+    KDBBANK,
+//    NH농협은행
     NONGHYEOP,
-    WOORI
+//    SC제일은행
+    SC,
+//    Sh수협은행
+    SUHYEOP
 }
