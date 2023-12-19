2da66a1a8e445f153219f10d7c06bbf1f8374977
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-12-20 03:45:29 +0900

    Feat: 토큰 인증 방식으로 변경

--- src/main/java/com/koreanguide/koreanguidebackend/domain/chat/controller/ChatController.java
+++ src/main/java/com/koreanguide/koreanguidebackend/domain/chat/controller/ChatController.java
@@ -1,26 +1,32 @@
 package com.koreanguide.koreanguidebackend.domain.chat.controller;
 
 import com.koreanguide.koreanguidebackend.common.BaseResponseDto;
+import com.koreanguide.koreanguidebackend.config.security.JwtTokenProvider;
 import com.koreanguide.koreanguidebackend.domain.chat.data.dto.CreateChatRoomRequestDto;
 import com.koreanguide.koreanguidebackend.domain.chat.data.dto.response.ChatListResponseDto;
 import com.koreanguide.koreanguidebackend.domain.chat.data.dto.response.ChatResponseDto;
 import com.koreanguide.koreanguidebackend.domain.chat.service.ChatService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/chat")
 public class ChatController {
     private final ChatService chatService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public ChatController(ChatService chatService) {
+    public ChatController(ChatService chatService, JwtTokenProvider jwtTokenProvider) {
         this.chatService = chatService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/create")
@@ -28,9 +34,13 @@ public class ChatController {
         return chatService.createChatRoom(createChatRoomRequestDto);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "X-AUTH-TOKEN", required = true,
+                    dataType = "String", paramType = "header")
+    })
     @GetMapping("/list")
-    public ResponseEntity<List<ChatListResponseDto>> getChatList(@RequestParam Long userId) {
-        return chatService.getChatList(userId);
+    public ResponseEntity<List<ChatListResponseDto>> getChatList(HttpServletRequest request) {
+        return chatService.getChatList(jwtTokenProvider.getUserIdByToken(request.getHeader("X-AUTH-TOKEN")));
     }
 
     @GetMapping("/")
--- src/main/java/com/koreanguide/koreanguidebackend/domain/track/controller/TrackController.java
+++ src/main/java/com/koreanguide/koreanguidebackend/domain/track/controller/TrackController.java
@@ -1,36 +1,58 @@
 package com.koreanguide.koreanguidebackend.domain.track.controller;
 
+import com.koreanguide.koreanguidebackend.config.security.JwtTokenProvider;
 import com.koreanguide.koreanguidebackend.domain.auth.data.dto.response.BaseResponseDto;
 import com.koreanguide.koreanguidebackend.domain.track.data.dto.request.TrackApplyRequestDto;
 import com.koreanguide.koreanguidebackend.domain.track.data.dto.response.TrackResponseDto;
 import com.koreanguide.koreanguidebackend.domain.track.service.TrackService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/v1/track")
 public class TrackController {
     private final TrackService trackService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public TrackController(TrackService trackService) {
+    public TrackController(TrackService trackService, JwtTokenProvider jwtTokenProvider) {
         this.trackService = trackService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "X-AUTH-TOKEN", required = true,
+                    dataType = "String", paramType = "header")
+    })
     @PostMapping("/")
-    public ResponseEntity<BaseResponseDto> applyTrack(@RequestParam Long userId,
+    public ResponseEntity<BaseResponseDto> applyTrack(HttpServletRequest request,
                                                       @RequestBody TrackApplyRequestDto trackApplyRequestDto) {
-        return trackService.applyTrack(userId, trackApplyRequestDto);
+        return trackService.applyTrack(jwtTokenProvider.getUserIdByToken(request.getHeader("X-AUTh-TOKEN")),
+                trackApplyRequestDto);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "X-AUTH-TOKEN", required = true,
+                    dataType = "String", paramType = "header")
+    })
     @GetMapping("/")
-    public ResponseEntity<TrackResponseDto> getTrackById(@RequestParam Long userId, Long trackId) {
-        return trackService.getTrackById(userId, trackId);
+    public ResponseEntity<TrackResponseDto> getTrackById(HttpServletRequest request, @RequestParam Long trackId) {
+        return trackService.getTrackById(
+                jwtTokenProvider.getUserIdByToken(request.getHeader("X-AUTH-TOKEN")), trackId);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "X-AUTH-TOKEN", required = true,
+                    dataType = "String", paramType = "header")
+    })
     @PostMapping("/visible")
-    public ResponseEntity<BaseResponseDto> changeTracksVisible(@RequestParam Long userId, Long trackId) {
-        return trackService.changeTracksVisible(userId, trackId);
+    public ResponseEntity<BaseResponseDto> changeTracksVisible(HttpServletRequest request, @RequestParam Long trackId) {
+        return trackService.changeTracksVisible(
+                jwtTokenProvider.getUserIdByToken(request.getHeader("X-AUTH-TOKEN")), trackId);
     }
 }
