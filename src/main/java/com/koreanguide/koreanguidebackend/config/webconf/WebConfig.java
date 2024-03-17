19376ff245e7b1a9d2405448939128ed3f7b0bf1
Yun-Hwan Jeon <yunsol267@gmail.com>
2024-03-18 05:26:51 +0900

    Update WebConfig.java

--- src/main/java/com/koreanguide/koreanguidebackend/config/webconf/WebConfig.java
+++ src/main/java/com/koreanguide/koreanguidebackend/config/webconf/WebConfig.java
@@ -1,20 +1,20 @@
 package com.koreanguide.koreanguidebackend.config.webconf;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowCredentials(false)
-                .maxAge(3000);
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+        resolver.setOneIndexedParameters(true);
+        argumentResolvers.add(resolver);
+        WebMvcConfigurer.super.addArgumentResolvers(argumentResolvers);
     }
 }
