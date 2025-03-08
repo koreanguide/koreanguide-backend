0d45776f5897f208160964e4a935df58fa09ea7a
Yun-Hwan Jeon <yunsol267@gmail.com>
2025-03-08 23:56:59 +0000

    🎉 Initialize: Gradle 프로젝트로 변경

---
+++ koreanguide-api/src/main/java/kr/yuns/KoreanguideApiApplication.java
@@ -0,0 +1,11 @@
+package kr.yuns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreanguideApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KoreanguideApiApplication.class, args);
+    }
+}
