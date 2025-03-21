e27680bf3d7063ffacf57fe7034459c6adeca8c9
Yun-Hwan Jeon <yunsol267@gmail.com>
2025-03-21 14:03:15 +0000

    ➖ 불필요 의존성 제거

--- koreanguide-api/src/main/java/kr/yuns/credit/CreditController.java
+++ koreanguide-api/src/main/java/kr/yuns/credit/CreditController.java
@@ -1,6 +1,5 @@
 package kr.yuns.credit;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
